86d2084de8d2158b614d84e06bd9a915f603617a
ZawYeWint <zawyewint@my.jcu.edu.au>
2022-11-25 17:38:39 +0800

    Update to files

---
+++ CH-1/MovieQuoteInfo.java
@@ -0,0 +1,8 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("I am flying!");
+        System.out.println("Titanic");
+        System.out.println("Rose DeWitt Bukater");
+        System.out.println("1997");
+    }
+}
